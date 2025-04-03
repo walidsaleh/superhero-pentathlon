e9a3b86c9909e2227aedef864e7cf5aec7a9b543
Walid Saleh Sánchez <superwal@gmail.com>
2025-04-03 12:46:01 +0200

    update layout

--- src/components/heroes/SuperheroCard.vue
+++ src/components/heroes/SuperheroCard.vue
@@ -19,34 +19,32 @@ defineEmits<{
 
 <template>
   <div
-    class="superhero-card group relative bg-white dark:bg-gray-900 rounded-lg shadow-lg dark:shadow-white/5 overflow-hidden hover:shadow-xl dark:hover:shadow-white/10 transition-shadow duration-200"
+    class="superhero-card group relative bg-white dark:bg-gray-900 rounded-lg border border-gray-300 dark:border-gray-700"
   >
-    <div class="h-48 flex items-center justify-center bg-gray-50 dark:bg-gray-800">
+    <div class="h-48 flex items-center justify-center bg-gray-50 dark:bg-gray-800 relative">
       <img
         :src="hero.image"
         :alt="hero.name"
         class="w-32 h-32 rounded-full object-cover shadow-md border-4 border-gray-300 dark:border-gray-700"
       />
-      <div
-        class="absolute inset-0 bg-white dark:bg-sky-950 opacity-0 group-hover:opacity-70 transition-opacity duration-200"
-      />
-      <div
-        class="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-200 flex items-center justify-center space-x-4"
-      >
-        <button
-          @click="$emit('edit', hero)"
-          class="p-2 bg-sky-600 dark:bg-sky-500/30 hover:bg-sky-700 dark:hover:bg-sky-500/90 rounded-full text-white transition-colors duration-200 cursor-pointer"
-          :title="t('pages.heroes.actions.edit')"
-        >
-          <PencilIcon class="w-5 h-5" />
-        </button>
+      <div class="ml-auto mt-2 mr-2 absolute inset-0 flex items-start justify-end space-x-4">
         <button
           @click="$emit('delete', hero)"
-          class="p-2 bg-sky-600 dark:bg-sky-500/30 hover:bg-sky-700 dark:hover:bg-sky-500/90 rounded-full text-white transition-colors duration-200 cursor-pointer"
+          class="mr-2 opacity-50 hover:opacity-100 rounded-full transition-opacity duration-200 cursor-pointer"
           :title="t('pages.heroes.actions.delete')"
         >
-          <ArrowPathIcon v-if="loadingDelete === hero.id" class="w-5 h-5 animate-spin" />
-          <TrashIcon v-else class="w-5 h-5" />
+          <ArrowPathIcon
+            v-if="loadingDelete === hero.id"
+            class="w-5 h-5 animate-spin text-gray-600 dark:text-gray-400"
+          />
+          <TrashIcon v-else class="w-5 h-5 text-gray-600 dark:text-gray-400" />
+        </button>
+        <button
+          @click="$emit('edit', hero)"
+          class="opacity-50 hover:opacity-100 rounded-full transition-opacity duration-200 cursor-pointer"
+          :title="t('pages.heroes.actions.edit')"
+        >
+          <PencilIcon class="w-5 h-5 text-gray-600 dark:text-gray-400" />
         </button>
       </div>
     </div>
