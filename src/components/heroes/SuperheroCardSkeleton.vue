549e9b904bb3b8ad06108fdc829e38fe8083fa5b
Walid Saleh Sánchez <superwal@gmail.com>
2025-04-04 12:49:49 +0200

    update skeleton with accurate layout structure

--- src/components/heroes/SuperheroCardSkeleton.vue
+++ src/components/heroes/SuperheroCardSkeleton.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { DEFAULT_ATTRIBUTES } from '@/types/superhero'
 import type { SuperheroAttributes } from '@/types/superhero'
 
-const SUPERHERO_ATTRIBUTES = Object.keys({} as SuperheroAttributes) as (keyof SuperheroAttributes)[]
+// Use DEFAULT_ATTRIBUTES to infer the keys
+const SUPERHERO_ATTRIBUTES_COUNT = Object.keys(DEFAULT_ATTRIBUTES) as (keyof SuperheroAttributes)[]
 </script>
 
 <template>
@@ -14,7 +16,7 @@ const SUPERHERO_ATTRIBUTES = Object.keys({} as SuperheroAttributes) as (keyof Su
     <div class="p-4 space-y-4">
       <div class="h-6 bg-gray-200 dark:bg-gray-700 rounded w-3/4"></div>
       <div class="space-y-2">
-        <div v-for="attr in SUPERHERO_ATTRIBUTES" :key="attr" class="flex items-center space-x-2">
+        <div v-for="i in SUPERHERO_ATTRIBUTES_COUNT" :key="i" class="flex items-center space-x-2">
           <div class="h-4 bg-gray-200 dark:bg-gray-700 rounded w-1/4"></div>
           <div class="h-4 bg-gray-200 dark:bg-gray-700 rounded flex-grow"></div>
         </div>
